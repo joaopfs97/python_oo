a736af32eca82fd980e212cb03476538905a93f2
joaopfs97 <joaopfs201@gmail.com>
2025-08-30 19:59:57 -0300

    Create Restaurante.py

---
+++ model/Restaurante.py
@@ -0,0 +1,22 @@
+class Restaurante:
+    restaurantes = []
+    def __init__(self, nome, categoria):  
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        if self.ativo:
+            return f'Nome: {self.nome} | Categoria: {self.categoria} | Situação: Ativo'
+        else:
+            return f'Nome: {self.nome} | Categoria: {self.categoria} | Situação: Inativo'
+    
+    def lista_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(restaurante)
+
+restaurante_praca = Restaurante('Praça','Gourmet')
+restarante_pizza = Restaurante('Pizza Premium','Italiana')
+
+Restaurante.lista_restaurantes()
